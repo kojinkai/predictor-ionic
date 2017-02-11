dff5f9562431b9fe332c9518766fb6cf882d919d
Lewis Nixon <lewcfn@gmail.com>
2017-02-11 11:23:45 +0000

    chore(theme): more basic styling of the fixtures page

---
+++ fixtures/fixtures.fixture.js
@@ -0,0 +1,139 @@
+export const FIXTURES = [
+  {
+    'name': 'Gameweek 1',
+    'fixtures': [
+      {
+        'home': 'Team 1',
+        'away': 'Team 2',
+      },
+      {
+        'home': 'Team 3',
+        'away': 'Team 4',
+      },
+      {
+        'home': 'Team 5',
+        'away': 'Team 6',
+      },
+    ],
+  },
+  {
+    'name': 'Gameweek 2',
+    'fixtures': [
+      {
+        'home': 'Team 1',
+        'away': 'Team 4',
+      },
+      {
+        'home': 'Team 2',
+        'away': 'Team 5',
+      },
+      {
+        'home': 'Team 3',
+        'away': 'Team 6',
+      },
+    ],
+  },
+
+  {
+    'name': 'Gameweek 3',
+    'fixtures': [
+      {
+        'home': 'Team 1',
+        'away': 'Team 5',
+      },
+      {
+        'home': 'Team 2',
+        'away': 'Team 6',
+      },
+      {
+        'home': 'Team 3',
+        'away': 'Team 4',
+      },
+    ],
+  },
+  {
+    'name': 'Gameweek 4',
+    'fixtures': [
+      {
+        'home': 'Team 1',
+        'away': 'Team 5',
+      },
+      {
+        'home': 'Team 2',
+        'away': 'Team 6',
+      },
+      {
+        'home': 'Team 3',
+        'away': 'Team 4',
+      },
+    ],
+  },
+  {
+    'name': 'Gameweek 5',
+    'fixtures': [
+      {
+        'home': 'Team 1',
+        'away': 'Team 5',
+      },
+      {
+        'home': 'Team 2',
+        'away': 'Team 6',
+      },
+      {
+        'home': 'Team 3',
+        'away': 'Team 4',
+      },
+    ],
+  },
+  {
+    'name': 'Gameweek 6',
+    'fixtures': [
+      {
+        'home': 'Team 1',
+        'away': 'Team 5',
+      },
+      {
+        'home': 'Team 2',
+        'away': 'Team 6',
+      },
+      {
+        'home': 'Team 3',
+        'away': 'Team 4',
+      },
+    ],
+  },
+  {
+    'name': 'Gameweek 7',
+    'fixtures': [
+      {
+        'home': 'Team 1',
+        'away': 'Team 5',
+      },
+      {
+        'home': 'Team 2',
+        'away': 'Team 6',
+      },
+      {
+        'home': 'Team 3',
+        'away': 'Team 4',
+      },
+    ],
+  },
+  {
+    'name': 'Gameweek 8',
+    'fixtures': [
+      {
+        'home': 'Team 1',
+        'away': 'Team 5',
+      },
+      {
+        'home': 'Team 2',
+        'away': 'Team 6',
+      },
+      {
+        'home': 'Team 3',
+        'away': 'Team 4',
+      },
+    ],
+  },
+];
