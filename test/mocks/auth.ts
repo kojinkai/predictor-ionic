330926a4e8a3051f67f9e83aee99bf49ce9d1a84
Lewis Nixon <lewcfn@gmail.com>
2017-05-17 09:42:00 +0100

    fix(auth): Update class to correctly implement interface

--- test/mocks/auth.ts
+++ test/mocks/auth.ts
@@ -45,6 +45,7 @@ class MockFirebaseUser implements firebase.User {
   isAnonymous: boolean;
   providerData: (firebase.UserInfo)[];
   refreshToken: string;
+  linkWithCredential: Promise<firebase.User>;
 
   delete() { return null; }
   getToken() { return null; }
